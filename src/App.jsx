331998bd1ae68f8297016f7dd427edabb2ddfd6d
Rodrigo <rwesley2000@gmail.com>
2025-09-02 23:10:40 -0300

    feat: useState added for nav-toggle and some styles to the List

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
-import './App.css'
-import Logo from './assets/logo.svg'
-import Lupa from './assets/lupa.svg'
+import React, { useState } from 'react';
+import './App.css';
+import Logo from './assets/logo.svg';
+import Lupa from './assets/lupa.svg';
 
 function App() {
+  const [navActive, setNavActive] = useState(false);
 
   return (
     <>
-      <div id='cabecalho' >
-        <h3 id='titulo' >Confira Nossos Produtos</h3>
+      <div id="cabecalho">
+        <h3 id="titulo">Confira Nossos Produtos</h3>
       </div>
 
-      <div id='search-box' >
-        <img id='logo' src={Logo} alt="" />
-        <p>Produtos</p> <p>contate-nos</p>
-        <div id='input-box' >
-          <input id='input' type="text" placeholder='pesquise aqui' />
-          <button id='btn-pesquisar' > <img src={Lupa} alt="" /></button>
+      <div id="search-box">
+        <nav className={`rotating-nav ${navActive ? 'active' : ''}`}>
+          <button
+            className="nav-toggle"
+            onClick={() => setNavActive(prev => !prev)}
+            aria-expanded={navActive}
+            aria-label="Abrir menu"
+          >
+            <span className="material-symbols-outlined">
+              shopping_cart
+            </span>
+          </button>
+
+          <ul className="nav-items">
+            <li><a href="#">Cadastrar Produtos</a></li>
+            <li><a href="#">Ver todos os Produtos</a></li>
+          </ul>
+        </nav>
+
+        <p>Produtos</p>
+        <p>contate-nos</p>
+
+        <div id="input-box">
+          <input id="input" type="text" placeholder="pesquise aqui" />
+          <button id="btn-pesquisar">
+            <img src={Lupa} alt="buscar" />
+          </button>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
