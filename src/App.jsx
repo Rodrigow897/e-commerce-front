998590c81e0c6770b32eb4eb2ed048e0fc9e4bbd
Rodrigo <rwesley2000@gmail.com>
2025-09-03 22:18:31 -0300

    feat: grid added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Logo from './assets/logo.svg';
+import Fone from './assets/fone.jpg'
 import Lupa from './assets/lupa.svg';
 
 function App() {
@@ -43,6 +43,15 @@ function App() {
           <button id="btn-pesquisar">
             <img src={Lupa} alt="buscar" />
           </button>
+        </div>
+      </div>
+
+      <div className="product-grid">
+        <div className="product-card">
+          <img src={Fone} alt="Product 1" className='product-img' />
+          <h3>Nome do produto</h3>
+          <p>$199.99</p>
+          <button className='add-to-cart' data-id="1" data-name="Nome do produto" data-price="199.99">Add ao Carrinho</button>
         </div>
       </div>
     </>
